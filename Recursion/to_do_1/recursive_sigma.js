168ebee657b419ea660ffd80007668b8ee252902
MSBivens <mikebiv95@gmail.com>
2022-05-18 20:27:30 -0500

    Set up for recursion assignments

---
+++ Recursion/to_do_1/recursive_sigma.js
@@ -0,0 +1,3 @@
+// Recursive Sigma
+// Write a recursive function that given a number returns the sum of integers from 1 to that number. 
+// Example: rSigma(5) = 15 (1+2+3+4+5); rSigma(2.5) = 3 (1+2); rSigma(-1) = 0.
